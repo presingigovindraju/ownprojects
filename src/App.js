b0247469e2d140f3683a8ad21151e0eeed58d01d
presingi_govind_raju <presingigovindraju333@gmail.com>
2024-04-11 12:07:33 +0530

    range

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Hello from "./Components/Hello";
+import Button from "./Components/Button";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+const App = ()=>{
+
+    
+    return (
+        <div>
+           <Hello />
+           <Button name="Raju"/>
+           <Button  name="Balu"/>
+           <Button  name="Vijay"/>
+        </div>
+    )
 }
 
-export default App;
+export default App
---
+++ src/Components/Hello.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+const Hello = ()=>{
+
+    let a=10;
+
+    return (
+        <div style={{backgroundColor:"red",color:"white",border:"9px solid green"}}>
+           <h1>hi man : {a}</h1>
+        </div>
+    )
+}
+
+
+export default Hello
